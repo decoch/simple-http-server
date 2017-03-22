7a72e8fcc67b0a3b0fc3dcb7730ac0f17f09c14c
Akifumi.Tominaga <tominaga.switch@gmail.com>
2017-03-22 22:34:11 +0900

    使いそうなパッケージをコメント

---
+++ src/HTTPServer/HTTPServer.scala
@@ -0,0 +1,11 @@
+package HTTPServer
+
+/**
+  * @author tominaga
+  */
+object HTTPServer {
+  // 使いそうなパッケージ
+  // /Library/Java/JavaVirtualMachines/jdk1.8.0_60.jdk/Contents/Home/src.zip!/java/net/ServerSocket.java
+  // 参考にしたURL
+  // https://twitter.github.io/scala_school/concurrency.html
+}
