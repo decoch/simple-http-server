65baa57942c8f427333a21b1bdb0cb32a615557b
Akifumi.Tominaga <tominaga.switch@gmail.com>
2017-03-25 12:02:51 +0900

    STEP7. URLに応じたHTMLを返せるようにしよう！

--- src/HTTPServer.scala
+++ src/HTTPServer.scala
@@ -9,18 +9,45 @@ import scala.collection.mutable.ArrayBuffer
   */
 object HTTPServer {
   def main(args: Array[String]): Unit = {
-    val br = new BufferedReader(new InputStreamReader(new FileInputStream("./src/response.html")))
-    val html = br.lines().toArray.mkString("\n")
+    val serverSocket = new ServerSocket(8000)
+    while (true) {
+      val socket = serverSocket.accept()
+      val url: String = extractUrl(socket.getInputStream)
+      val response: String = buildResponse(url)
+      socket.getOutputStream.write(response.getBytes)
+      socket.close()
+    }
+  }
 
-    val socket = new ServerSocket(8000).accept()
-    val out = socket.getOutputStream
+  private def extractUrl(input: InputStream): String = {
+    val br = new BufferedReader(new InputStreamReader(input))
+    val reqestLine = br.readLine()
+    val requestArray = reqestLine.split(" ")
+    requestArray(1)
+  }
+
+  private def buildResponse(url: String): String = {
+    val filePath = "./src" + url
+    val file = new File(filePath)
+    val contentType = "Content-Type: text/html"
+    val date = "%ta, %<td %<tb %<tY %<tT %<tz" formatLocal(Locale.ENGLISH, new Date)
+
+    if (!file.exists() || !file.isFile) {
+      val response = new ArrayBuffer[String]
+      response += "HTTP/1.1 404 Not Found"
+      response += contentType
+      response += date
+      return response.toList.mkString("\n")
+    }
+
+    val fileBr = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))
+    val body = fileBr.lines().toArray.mkString("\n")
     val response = new ArrayBuffer[String]
     response += "HTTP/1.1 200 OK"
-    response += "Content-Type: text/html"
-    response += "%ta, %<td %<tb %<tY %<tT %<tz" formatLocal(Locale.ENGLISH, new Date)
+    response += contentType
+    response += date
     response += ""
-    response += html
-    val result = response.toList.mkString("\n")
-    out.write(result.getBytes)
+    response += body
+    response.toList.mkString("\n")
   }
 }
